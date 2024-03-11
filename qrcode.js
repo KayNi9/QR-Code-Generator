3ec5f595be7a05045348c171a457b0d265bce1ee
Omkant <153151679+KayNi9@users.noreply.github.com>
2024-03-11 13:28:14 +0530

    Update qrcode.js

--- qrcode.js
+++ qrcode.js
@@ -1,10 +1,10 @@
 let form=document.querySelector("form")
 let sec=document.querySelector("section")
+let image=document.createElement("img")
 
 form.addEventListener("submit",(a)=>{
     a.preventDefault()
     let input=document.querySelector("#url").value
-    let image=document.createElement("img")
     let url=`https://chart.googleapis.com/chart?cht=qr&chs=400x400&chl=${input}`
     image.src=url
     sec.appendChild(image)
